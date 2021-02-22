69e4db2b50d0654b8e2e0ffb971e8d2822a4442f
Raj Kale <kale44772@gmail.com>
2021-02-22 20:54:51 +0530

    initial and firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyAtvVkusHyem1Nj1Cunv8kQLswOYCXA8Q8",
+    authDomain: "netflix-redux-25d64.firebaseapp.com",
+    projectId: "netflix-redux-25d64",
+    storageBucket: "netflix-redux-25d64.appspot.com",
+    messagingSenderId: "801180449403",
+    appId: "1:801180449403:web:915e3f028968b4a8cb80de",
+    measurementId: "G-4NFSRB8QSR"
+  };
