57fdec9dbb26793f1c1886f9d2bb2cbb769e66b3
Raj Kale <kale44772@gmail.com>
2021-02-24 13:27:04 +0530

    Start of Home Screen

---
+++ src/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './HomeScreen'
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            HomeScreen
+        </div>
+    )
+}
+
+export default HomeScreen
